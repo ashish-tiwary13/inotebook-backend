25e23063621d4efe3368566249149d2ceca11b04
ashish-tiwary13 <ashishtiwary1326@gmail.com>
2023-02-13 04:50:29 +0530

    try

--- index.js
+++ index.js
@@ -19,17 +19,17 @@ app.use('/api/note', require('./routes/note'))
 
 // deployment
 
-__dirname = path.resolve();
-if(process.env.NODE_ENV ==='production'){
-  app.use(express.static(path.join(__dirname,"/frontend/build")));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'frontend','build',"index.html"));
-  });
-}else{
+// __dirname = path.resolve();
+// if(process.env.NODE_ENV ==='production'){
+//   app.use(express.static(path.join(__dirname,"/frontend/build")));
+//   app.get('*',(req,res)=>{
+//     res.sendFile(path.resolve(__dirname,'frontend','build',"index.html"));
+//   });
+// }else{
   app.get("/",(req,res)=>{
     res.send("API is running..");
   });
-}
+// }
 //
 
 app.listen(PORT, () => {
